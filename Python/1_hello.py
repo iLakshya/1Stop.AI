0a287243ba923b9ab7144ec64cf7422f3e315563
Lakshya Goel <lgoel340@gmail.com>
2023-06-01 14:21:21 +0530

    Hello World and Comments Introduction

---
+++ Python/1_hello.py
@@ -0,0 +1,13 @@
+# First Python Program
+
+print('hello world')
+print("hello world")
+
+
+# Comments are not only for the beginners but also for the developers to mark something important or for code review
+# Comments are important to explain what the code is doing
+
+# There are 2 types of comments ----- Multi line (""") and single line (#)
+"""
+this is a multi line comment
+"""
