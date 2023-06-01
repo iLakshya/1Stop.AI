c7b2081db439ee31ad4c2236c3a5cd204f6b0a98
Lakshya Goel <lgoel340@gmail.com>
2023-06-01 15:13:26 +0530

    Basic Data Types

---
+++ Python/2_datatypes.py
@@ -0,0 +1,35 @@
+# Numbers --> integers, numbers, float
+# type() is a function used to tell us the type of the data which is used for the variable
+# print(type(x)) prints the data type
+x = 8
+print(type(x))
+
+X = 10.0
+print(type(X))
+
+a = -5.34
+print(type(a))
+
+c = -4
+print(type(c))
+
+# help(str)      # for the string
+
+z = "hi there! am a string"
+print(type(z))
+z1 = 'hi there! am a string'
+print(type(z1))
+z2 = '1'
+print(type(z2))
+
+state = "Mary's dog said 'Woof!'"
+print(state)
+
+state1 = "Mart's dog said \"Woof\""       # \""\ is an scape sequence
+print(state1)
+
+line = "Hello \nWorld!"
+print(line)
+
+boo = True
+print(type(boo))
