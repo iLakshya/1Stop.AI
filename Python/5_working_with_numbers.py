9a0a09f6403e2db2a5cd55d0c26df0a6186ddfc7
Lakshya Goel <lgoel340@gmail.com>
2023-06-02 14:17:38 +0530

    Working with Numbers

---
+++ Python/5_working_with_numbers.py
@@ -0,0 +1,34 @@
+# Prints the absolute value of the number
+print(abs(8))
+print(abs(-8))
+
+print(abs(8.0))
+print(abs(-8.0))
+
+print(abs(9.1))
+print(abs(-9.1))
+
+print(abs(7.8))
+print(abs(-7.8))
+
+# Converts the number entered to the respective bases
+print(bin(21))
+print(oct(21))
+print(hex(21))
+
+# Converts the number to the integer of the integer range
+print(int(4.4))
+print(int(4.9))
+print(int(4))
+
+# Converts the number to the float of the float range
+print(float(4))
+print(float(7.8))
+
+# Rounds off the number to the nearest integer
+print(round(4.5))
+print(round(4.6))
+
+# Maximum and Minimum returns which are the maximum and minimum values in the list
+print(max(1,2,3,4,4,3))
+print(min(-1,-5,-1,3))
