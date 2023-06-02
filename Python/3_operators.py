31e9dd661f19587f87c7db09ceb5695fe48cb2f1
Lakshya Goel <lgoel340@gmail.com>
2023-06-02 10:39:41 +0530

    Operators in Python

---
+++ Python/3_operators.py
@@ -0,0 +1,34 @@
+a = 8
+b = 3
+
+# Arithmetic Operations: + , - , * , / , // , ** , %
+print(a + b)
+print(a - b)
+print(a * b)
+print(a % b)
+print(a**b)
+print(a/b)
+print(a//b)
+
+# Comparison operators: < , > , <= , >= , == , != , is , is not
+print(a<b)
+print(a>b)
+print(a<=b)
+print(a>=b)
+print(a==b)
+print(a!=b)
+print(a is b)
+print(a is not b)
+
+# Boolean Operations: and , or , not
+print(a or b)
+print(a and b)
+print(not a)
+
+# Extra
+print(3 >= 3.0)
+print(3 >= 3.5)
+print("abcd"=="abcd")
+print("abcd"=="abCd")
+print(len('abcd')==len("adss"))
+print(len('abcd')==len('ades'))
