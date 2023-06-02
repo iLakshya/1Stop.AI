ed30ddc6b0e2181d5cfa1ea82beb1b8601eb75f1
Lakshya Goel <lgoel340@gmail.com>
2023-06-02 12:59:24 +0530

    Variables in Python

---
+++ Python/4_variables.py
@@ -0,0 +1,9 @@
+a = 5; b = 6; c = 10; print(a,b,c)
+# we don't use ; in python but when multiple statements are used in single then they are separated by ;
+
+prefix = "Sir"; first_name = 'Ratan'; last_name = "Tata"
+print(prefix,first_name,last_name)
+
+# multiple declarations can also be done
+m, n = 1, 2
+print(m,n)
