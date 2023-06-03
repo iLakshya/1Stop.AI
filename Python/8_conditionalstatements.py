ece34a3f7c040360725d91b69592dc27d4db9022
Lakshya Goel <lgoel340@gmail.com>
2023-06-03 20:00:49 +0530

    Conditional Statements

---
+++ Python/8_conditionalstatements.py
@@ -0,0 +1,27 @@
+# if condition example
+sun = 'down'
+if sun == 'down' :
+    print("good night")
+print("End of program 1")
+
+sunn = 'not down'
+if sunn == 'down':
+    print("good morning")
+print("End of program 2")
+
+# else condition example
+sunnn = 'down'
+if sunnn == 'down':
+    print("good evening")
+else:
+    print("good night")
+print("End of program 3")
+
+# elif condition example
+sun_set = 'yes'
+if sun_set == 'no':
+    print("hemlo")
+elif sun_set == 'yes':
+    print("welcomee")
+else:
+    print("End of program 4")
